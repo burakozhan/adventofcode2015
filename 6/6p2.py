ace1adbd3dd9ecf54431fd9468ef8bd731f6e6a0
Burak Özhan <burak@ozhan.de>
2016-03-14 23:11:54 +0100

    Day 6: Probably a Fire Hazard

---
+++ 6/6p2.py
@@ -0,0 +1,65 @@
+from sys import argv
+import numpy
+
+f = open(argv[1])
+
+lines = f.readlines()
+
+grid = numpy.zeros((1000,1000))
+
+def getxy(combined):
+	xy = combined.split(",")
+	x = int(xy[0])
+	y = int(xy[1])
+	return (x,y)
+
+def ton(command):
+	data = command.split()
+	topleft = data[2]
+	rightbottom = data[4]
+	tl = getxy(topleft)
+	br = getxy(rightbottom)
+	global grid
+	for x in range(tl[0],br[0]+1):
+		for y in range(tl[1],br[1]+1):
+			grid[x][y] += 1
+
+def tog(command):
+	data = command.split()
+	topleft = data[1]
+	rightbottom = data[3]
+	tl = getxy(topleft)
+	br = getxy(rightbottom)
+	global grid
+	for x in range(tl[0],br[0]+1):
+		for y in range(tl[1],br[1]+1):
+			grid[x][y] +=2
+
+def tof(command):
+	data = command.split()
+	topleft = data[2]
+	rightbottom = data[4]
+	tl = getxy(topleft)
+	br = getxy(rightbottom)
+#	print " "+ str(tl[0]) + " "+ str(br[0])
+#	print " "+ str(tl[1]) + " "+ str(br[1])
+	global grid
+	for x in range(tl[0],br[0]+1):
+		for y in range(tl[1],br[1]+1):
+			if(grid[x][y] > 0):
+				grid[x][y] -= 1
+#			print "turning off x = " + str(x) + " y = " + str(y)
+
+ctr = 1
+
+for line in lines:
+	print str(ctr)+" processing: "+ line.replace("\n",".")
+	ctr += 1
+	if line.startswith("turn on"):
+		ton(line)
+	if line.startswith("toggle"):
+		tog(line)
+	if line.startswith("turn off"):
+		tof(line)
+
+print str(int(numpy.sum(grid)))
